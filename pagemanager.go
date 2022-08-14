884fc8c4b6b339329df12ccb2b27b3d62d3c4498
bokwoon <bokwoon.c@gmail.com>
2022-08-14 23:15:59 +0800

    checkpoint

--- pagemanager.go
+++ pagemanager.go
@@ -3,8 +3,10 @@ package pagemanager
 import (
 	"bytes"
 	"database/sql"
+	"fmt"
 	"io/fs"
 	"net/http"
+	"runtime"
 	"sync"
 )
 
@@ -29,6 +31,7 @@ type Route struct {
 type Pagemanager struct {
 	Mode     int
 	FS       fs.FS
+	Dialect  string
 	DB1      *sql.DB
 	DB2      *sql.DB
 	DB3      *sql.DB
@@ -36,10 +39,32 @@ type Pagemanager struct {
 	sources  map[string]func(route *Route, args ...string) (any, error)
 }
 
-func RegisterInit(init func() error) {
+var (
+	initFuncsMu sync.RWMutex
+	initFuncs   []func(*Pagemanager) error
+)
+
+func RegisterInit(init func(*Pagemanager) error) {
+	initFuncsMu.Lock()
+	defer initFuncsMu.Unlock()
+	if init == nil {
+		_, file, line, _ := runtime.Caller(1)
+		panic(fmt.Sprintf("pagemanager: %s:%d: init function is nil", file, line))
+	}
+	initFuncs = append(initFuncs, init)
 }
 
+var (
+	sourcesMu sync.RWMutex
+	sources   map[string]func(*Pagemanager) func(*Route, ...string) (any, error)
+)
+
 func RegisterSource(name string, constructor func(*Pagemanager) func(*Route, ...string) (any, error)) {
+	sourcesMu.Lock()
+	defer sourcesMu.Unlock()
+	if constructor == nil {
+		panic(fmt.Sprintf("pagemanager: RegisterHandler handler is nil"))
+	}
 }
 
 func RegisterHandler(name string, constructor func(*Pagemanager) http.Handler) {
