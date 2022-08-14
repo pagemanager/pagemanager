0483e60ac91acb30cbe43c8821cadbc1dbca7c19
bokwoon <bokwoon.c@gmail.com>
2022-08-15 02:23:06 +0800

    checkpoint

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.18
 
 require (
 	github.com/google/go-cmp v0.5.8
+	github.com/mattn/go-sqlite3 v1.14.14
 	github.com/yuin/goldmark v1.4.13
 	github.com/yuin/goldmark-highlighting v0.0.0-20220208100518-594be1970594
 )
--- pagemanager.go
+++ pagemanager.go
@@ -11,9 +11,10 @@ import (
 	"net/url"
 	"os"
 	"path/filepath"
-	"runtime"
 	"strings"
 	"sync"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
 const (
@@ -46,18 +47,22 @@ type Pagemanager struct {
 }
 
 var (
-	initFuncsMu sync.RWMutex
-	initFuncs   []func(*Pagemanager) error
+	initFuncsMu   sync.RWMutex
+	initFuncs     []func(*Pagemanager) error // TODO: make this a map, sort the keys before iterating. If a plugin die die need to initialize first, they can give their name a very high sort priority.
+	initFuncNames = make(map[string]struct{})
 )
 
-func RegisterInit(init func(*Pagemanager) error) {
+func RegisterInit(name string, initFunc func(*Pagemanager) error) {
 	initFuncsMu.Lock()
 	defer initFuncsMu.Unlock()
-	if init == nil {
-		_, file, line, _ := runtime.Caller(1)
-		panic(fmt.Sprintf("pagemanager: %s:%d: init function is nil", file, line))
+	if _, dup := initFuncNames[name]; dup {
+		panic(fmt.Sprintf("pagemanager: RegisterInit called twice for init function %q", name))
 	}
-	initFuncs = append(initFuncs, init)
+	if initFunc == nil {
+		panic(fmt.Sprintf("pagemanager: RegisterInit %q init function is nil", name))
+	}
+	initFuncNames[name] = struct{}{}
+	initFuncs = append(initFuncs, initFunc)
 }
 
 var (
@@ -68,11 +73,11 @@ var (
 func RegisterSource(name string, constructor func(*Pagemanager) func(*Route, ...string) (any, error)) {
 	sourcesMu.Lock()
 	defer sourcesMu.Unlock()
-	if constructor == nil {
-		panic(fmt.Sprintf("pagemanager: RegisterSource %q source is nil", name))
-	}
 	if _, dup := sources[name]; dup {
 		panic(fmt.Sprintf("pagemanager: RegisterSource called twice for source %q", name))
+	}
+	if constructor == nil {
+		panic(fmt.Sprintf("pagemanager: RegisterSource %q source is nil", name))
 	}
 	sources[name] = constructor
 }
@@ -85,11 +90,11 @@ var (
 func RegisterHandler(name string, constructor func(*Pagemanager) http.Handler) {
 	handlersMu.Lock()
 	defer handlersMu.Unlock()
-	if constructor == nil {
-		panic(fmt.Sprintf("pagemanager: RegisterHandler %q handler is nil", name))
-	}
 	if _, dup := handlers[name]; dup {
 		panic(fmt.Sprintf("pagemanager: RegisterHandler called twice for handler %q", name))
+	}
+	if constructor == nil {
+		panic(fmt.Sprintf("pagemanager: RegisterHandler %q handler is nil", name))
 	}
 	handlers[name] = constructor
 }
@@ -103,15 +108,15 @@ var (
 )
 
 type Config struct {
-	Mode     int
-	FS       fs.FS
-	Dialect  string
-	Driver   string
-	DB1      *sql.DB
-	DB2      *sql.DB
-	DB3      *sql.DB
-	Handlers map[string]http.Handler
-	Sources  map[string]func(route *Route, args ...string) (any, error)
+	Mode       int
+	FS         fs.FS
+	Dialect    string
+	DriverName string
+	DB1        *sql.DB
+	DB2        *sql.DB
+	DB3        *sql.DB
+	Handlers   map[string]http.Handler
+	Sources    map[string]func(route *Route, args ...string) (any, error)
 }
 
 func normalizeDSN(c *Config, dsn string) (normalizedDSN string) {
@@ -163,10 +168,10 @@ func normalizeDSN(c *Config, dsn string) (normalizedDSN string) {
 			return dsn
 		}
 	}
-	if c.Driver == "" {
+	if c.DriverName == "" {
 		switch c.Dialect {
 		case "sqlite":
-			c.Driver = "sqlite3"
+			c.DriverName = "sqlite3"
 			dsn = strings.TrimPrefix(strings.TrimPrefix(dsn, "sqlite:"), "//")
 			before, after, _ := strings.Cut(dsn, "?")
 			q, err := url.ParseQuery(after)
@@ -178,7 +183,7 @@ func normalizeDSN(c *Config, dsn string) (normalizedDSN string) {
 			}
 			return before + "?" + q.Encode()
 		case "postgres":
-			c.Driver = "postgres"
+			c.DriverName = "postgres"
 			before, after, _ := strings.Cut(dsn, "?")
 			q, err := url.ParseQuery(after)
 			if err != nil {
@@ -189,10 +194,66 @@ func normalizeDSN(c *Config, dsn string) (normalizedDSN string) {
 			}
 			return before + "?" + q.Encode()
 		case "mysql":
-			c.Driver = "mysql"
-			return strings.TrimPrefix(dsn, "mysql://")
+			c.DriverName = "mysql"
+			if strings.HasPrefix(dsn, "mysql://") {
+				u, err := url.Parse(dsn)
+				if err != nil {
+					dsn = strings.TrimPrefix(dsn, "mysql://")
+				} else {
+					var b strings.Builder
+					b.Grow(len(dsn))
+					if u.User != nil {
+						username := u.User.Username()
+						password, ok := u.User.Password()
+						b.WriteString(username)
+						if ok {
+							b.WriteString(":" + password)
+						}
+					}
+					if u.Host != "" {
+						if b.Len() > 0 {
+							b.WriteString("@")
+						}
+						b.WriteString("tcp(" + u.Host + ")")
+					}
+					b.WriteString("/" + strings.TrimPrefix(u.Path, "/"))
+					if u.RawQuery != "" {
+						b.WriteString("?" + u.RawQuery)
+					}
+					dsn = b.String()
+				}
+			}
+			before, after, _ := strings.Cut(dsn, "?")
+			q, err := url.ParseQuery(after)
+			if err != nil {
+				return dsn
+			}
+			if !q.Has("allowAllFiles") {
+				q.Set("allowAllFiles", "true")
+			}
+			if !q.Has("multiStatements") {
+				q.Set("multiStatements", "true")
+			}
+			if !q.Has("parseTime") {
+				q.Set("parseTime", "true")
+			}
+			return before + "?" + q.Encode()
 		case "sqlserver":
-			c.Driver = "sqlserver"
+			c.DriverName = "sqlserver"
+			u, err := url.Parse(dsn)
+			if err != nil {
+				return dsn
+			}
+			if u.Path != "" {
+				before, after, _ := strings.Cut(dsn, "?")
+				q, err := url.ParseQuery(after)
+				if err != nil {
+					return dsn
+				}
+				q.Set("database", u.Path[1:])
+				dsn = strings.TrimSuffix(before, u.Path) + "?" + q.Encode()
+			}
+			return dsn
 		}
 	}
 	return dsn
@@ -228,10 +289,68 @@ func New(c *Config) (*Pagemanager, error) {
 		pm.FS = os.DirFS(dir) // TODO: eventually need to replace this with custom DirFS that allows WriteFile and stuff.
 	}
 	if pm.DB1 == nil && *pmDSN1 != "" {
-		dsn1 := normalizeDSN(c, *pmDSN1)
-		_ = dsn1
-		if c.Dialect == "" {
+		dsn := normalizeDSN(c, *pmDSN1)
+		if c.DriverName == "" {
+			return nil, fmt.Errorf("could not identity dialect for -pm-dsn1 %q", *pmDSN1)
 		}
+		pm.DB1, err = sql.Open(c.DriverName, dsn)
+		if err != nil {
+			return nil, fmt.Errorf("error connecting to %q: %w", *pmDSN1, err)
+		}
+	}
+	if pm.DB2 == nil && *pmDSN2 != "" {
+		dsn := normalizeDSN(c, *pmDSN2)
+		if c.DriverName == "" {
+			return nil, fmt.Errorf("could not identity dialect for -pm-dsn2 %q", *pmDSN2)
+		}
+		pm.DB2, err = sql.Open(c.DriverName, dsn)
+		if err != nil {
+			return nil, fmt.Errorf("error connecting to %q: %w", *pmDSN2, err)
+		}
+	}
+	if pm.DB3 == nil && *pmDSN3 != "" {
+		dsn := normalizeDSN(c, *pmDSN3)
+		if c.DriverName == "" {
+			return nil, fmt.Errorf("could not identity dialect for -pm-dsn3 %q", *pmDSN3)
+		}
+		pm.DB3, err = sql.Open(c.DriverName, dsn)
+		if err != nil {
+			return nil, fmt.Errorf("error connecting to %q: %w", *pmDSN3, err)
+		}
+	}
+	if pm.DB1 == nil && dir != "" {
+		c.Dialect = "sqlite"
+		c.DriverName = "sqlite3"
+		dsn := filepath.ToSlash(dir + "/pagemanager.db")
+		pm.DB1, err = sql.Open(c.DriverName, dsn)
+		if err != nil {
+			return nil, fmt.Errorf("error connecting to %q: %w", dsn, err)
+		}
+		pm.DB2 = pm.DB1
+		pm.DB3 = pm.DB1
+	}
+	if pm.DB1 != nil {
+		return nil, fmt.Errorf("database not provided")
+	}
+	if pm.DB2 == nil {
+		pm.DB2 = pm.DB1
+	}
+	if pm.DB3 == nil {
+		pm.DB3 = pm.DB1
+	}
+	if pm.handlers == nil || pm.sources == nil {
+		initFuncsMu.RLock()
+		defer initFuncsMu.RUnlock()
+		for _, initFunc := range initFuncs {
+			err = initFunc(pm)
+			if err != nil {
+				return nil, fmt.Errorf("error connecting to %q: %w", dsn, err)
+			}
+		}
+	}
+	if pm.handlers == nil {
+	}
+	if pm.sources == nil {
 	}
 	return pm, nil
 }
